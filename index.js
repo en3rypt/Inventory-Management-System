0eb575d744dacf7c19a70ff755217c5f1c953182
20PW14 <karthickprnv@gmail.com>
2022-07-14 07:34:40 +0530

    add ejs and bodyparsing

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 //express project setup
 const express = require('express');
-const db = require('./dbConnection');
-require('dotenv').config()
-
-
-
 const app = express();
-const port = 3000;
+const PORT = 3000;
+// const db = require('./dbConnection');
+require('dotenv').config()
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
 
 //home route
 app.get('/', (req, res) => res.send('Hello World!'));
 
+
+app.get('/signup', (req, res) => {
+    res.render('pages/signup');
+})
+
+app.post('/signup', (req, res) => {
+    res.send(`The Name is ${req.body.suName} and the email is ${req.body.suEmail}`);
+})
+
+
+
+
 //app listen
-app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Example app listening on http://localhost:${PORT}`));

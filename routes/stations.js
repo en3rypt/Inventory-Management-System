b17913dbebf98d2703073973a3073a3525208638
Akash20PW02 <20pw02@psgtech.ac.in>
2022-07-25 22:00:27 +0530

    Schemes and Stations added.

---
+++ routes/stations.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const stations = express.Router();
+const db = require('../dbConnection');
+
+stations.get('/new', (req, res) => {
+    res.render('pages/index', { option: 'newStation' });
+})
+stations.get('/', (req, res) => {
+
+    db.query(`SELECT * FROM categories`, (err, result) => {
+        // console.log(result);
+        if (err) {
+            throw err;
+        }
+        res.render('pages/index', { option: "stations", categoriesData: result });
+    });
+})
+
+// categories.post('/', (req, res) => {
+//     db.query(
+//         `INSERT INTO categories (Name) VALUES ('${req.body.categname}')`,
+//         (err, result) => {
+//             if (err) {
+//                 throw err;
+//             }
+//             res.redirect('/categories');
+//         }
+//     );
+// })
+module.exports = stations;
